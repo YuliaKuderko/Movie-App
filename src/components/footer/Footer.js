7e68d40ca58637ef8d565569a79362b6c986e3ab
Yulia Kuderko <yulia2232@gmail.com>
2023-02-20 21:24:10 +0200

    changed design of footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import './footer.scss'
 import { Link } from 'react-router-dom'
+import footerImg from '../../assets/footer-img.jpg'
 
 function Footer() {
   return (
-    <div className='footer'>
-        <div className='footer__content__logo'>
+    <div className='footer' style={{backgroundImage:`url(${footerImg})`}}>
+
           <div className='logo'>
             <Link to="/">app logo</Link>
           </div>
-        </div>
+
         <div className='footer__content__menus'>
           <div className='footer__content__menu'>
             <Link to='/'>Home</Link>
