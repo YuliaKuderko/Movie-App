d0dc71df055be2cc22c4efabf9460eb281da81a5
Yulia Kuderko <yulia2232@gmail.com>
2023-02-22 00:37:31 +0200

    added app icon+logo

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,12 +2,13 @@ import React from 'react'
 import './footer.scss'
 import { Link } from 'react-router-dom'
 import footerImg from '../../assets/footer-img.jpg'
+import logoImg from '../../assets/logo.png'
 
 function Footer() {
   return (
     <div className='footer' style={{ backgroundImage: `url(${footerImg})` }}>
       <div className='footer-logo'>
-        <Link to="/">app logo</Link>
+        <Link to="/">MOVIE APP</Link>
       </div>
       <div className='footer__content__menus'>
         <div className='footer__content__menu'>
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from 'react'
 import './header.scss'
 import { Link, useLocation } from 'react-router-dom'
+import logoImg from '../../assets/logo.png'
 
 const headerNav = [
   {
@@ -46,9 +47,11 @@ function Header() {
 
   return (
     <div ref={headerRef} className="header">
-        <div className='header-logo'>
-          <Link to="/" onClick={scrollToTop}>app logo</Link>
-        </div>
+        <Link className='header-logo' to="/" onClick={scrollToTop}>
+          <img src={logoImg} />
+          
+        </Link>
+     
       <ul className='header__nav'>
         {headerNav.map((e, i) => (
           <li key={i} className={i === active ? 'active' : ''}>
