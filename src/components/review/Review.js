3e38f2d217fce35408262cad36a9b41ae353a8ff
Yulia Kuderko <yulia2232@gmail.com>
2023-02-28 17:12:38 +0200

    no reviews case is implemented

--- src/components/review/Review.js
+++ src/components/review/Review.js
@@ -23,13 +23,13 @@ function Review(props) {
 
     return (
         <>
-            {reviews.map((item, i) => (
+            {reviews.length !== 0 ? reviews.map((item, i) => (
                 <div key={i} className='review'>
                     <div className='author-details'>
                         <img src={(item.author_details.avatar_path !== null && !item.author_details.avatar_path.includes("http")) ? apiConfig.w500Image(item.author_details.avatar_path) : 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'} className='review-avatar' />
                         <div>
-                            <h3>{item.author}</h3>
-                            <span style={{fontSize:"12px", color:"rgba(255, 255, 255, 0.854)"}}>{new Date(item.updated_at || item.created_at).toLocaleString()}</span>
+                            <h3 style={{fontSize:"18px"}}>{item.author}</h3>
+                            <span style={{ fontSize: "12px", color: "rgba(255, 255, 255, 0.854)" }}>{new Date(item.updated_at || item.created_at).toLocaleString()}</span>
                         </div>
                     </div>
                     <Box
@@ -48,14 +48,14 @@ function Review(props) {
                             precision={0.5}
                             size='medium'
                             emptyIcon={
-                                <StarBorderIcon fontSize="inherit" style={{color: "orange", opacity:0.7}} />
+                                <StarBorderIcon fontSize="inherit" style={{ color: "orange", opacity: 0.7 }} />
                             }
                         />
                         <Box sx={{ ml: 1, mt: 0.1, fontSize: 12, color: 'grey', textAlign: 'center', fontWeight: 500 }}>{item.author_details.rating || "0"} / 10</Box>
                     </Box>
                     <p className='review-content'>{item.content}</p>
                 </div>
-            ))}
+            )) : <div style={{textAlign:"center"}}>No reviews yet</div>}
         </>
     )
 }
