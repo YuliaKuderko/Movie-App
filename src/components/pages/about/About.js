1a4fd1dbc530dc0213db27880fb9385f26493ba1
Yulia Kuderko <yulia2232@gmail.com>
2023-03-05 16:35:50 +0200

    margin img

--- src/components/pages/about/About.js
+++ src/components/pages/about/About.js
@@ -17,7 +17,7 @@ export default function FAQ() {
                             <p className='about-paragraph'>I'm a fan at heart.
                                 Of movies, TV shows and well, all things entertainment. So much so that I built an app that brings it all together—Trailes, lists of the top rated and trending movies and series, ratings and search option. As beautiful as it is easy-to-use, Movie App gives users everywhere a way to discover and enjoy the entertainment they love the most.</p>
                         </div>
-                        <img src={gif1} height="350px" />
+                        <img src={gif1} height="350px" style={{marginBottom:"50px"}}/>
                     </div>
                 </div>
             </div>
